eb7a935438e70f47c412fede5a499ebbc699f56d
JRight <1576422606@qq.com>
2017-06-04 22:34:56 +0800

    完成搭建

--- app1.py
+++ app1.py
@@ -1,7 +1,6 @@
 # -*- coding:utf-8 -*-
-
+#老师写的
 import logging; logging.basicConfig(level=logging.INFO)
-#注意分号和空格
 
 import asyncio, os, json, time
 from datetime import datetime
@@ -9,13 +8,15 @@ from datetime import datetime
 from aiohttp import web
 
 def index(request):
-    return web.Response(body=b'<h1>Awsome</h1>')
+    return web.Response(body=b'<h1>Awesome</h1>')
 
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
